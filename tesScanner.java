c478488972ad5fac09e4506afcfac6cd4fde558e
Kihil <mirz1723@gmail.com>
2021-08-13 22:13:28 +0700

    Test Scanner

---
+++ tesScanner.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.lang.Object.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.Scanner;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+class theSolution {
+
+    Scanner theScanner = new Scanner(System.in);
+    //int n;
+    //int i;
+    int nofElement;
+
+    
+    void calculation() {
+
+        nofElement = theScanner.nextInt();
+        System.out.println(nofElement);
+    }
+    
+
+}
+
+public static void main(String[] args) {
+    theSolution theAnswer = new theSolution();
+    
+}
