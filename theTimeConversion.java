98d904e78c03d6c181477f3e1ff533ec62b10352
Kihil <mirz1723@gmail.com>
2021-08-19 08:21:20 +0700

    start timeConversion

---
+++ theTimeConversion.java
@@ -0,0 +1,12 @@
+public class theTimeConversion {
+
+    public static void theTimeConversion(){
+        
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+    
+}
