5b1a313877f507ca35c3662c6441cbe7a8ec5aed
Kihil <mirz1723@gmail.com>
2021-08-21 13:53:09 +0700

    queries elements is not yet applied

--- sparseArrays.java
+++ sparseArrays.java
@@ -11,6 +11,10 @@ import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+import java.lang.Object;
 
 public class sparseArrays {
 
@@ -26,75 +30,70 @@ public class sparseArrays {
         Scanner sc = new Scanner(System.in); // the scanner
 
         // Take input: size of strings
-        System.out.print("1. Input the size of strings: ");
+        // System.out.print("1. Input the size of strings: ");
         n = sc.nextInt();
 
         // Iterator 12 lines
-        System.out.println("2. Input the strings elements: ");
+        // System.out.println("2. Input the strings elements: ");
         for (i = 0; i <= n; i++) {
             // Take input: elements of strings
             strings.add(sc.nextLine());
         }
 
         // Take input: size of queries
-        System.out.print("3. Input the size of queries: ");
+        // System.out.print("3. Input the size of queries: ");
         m = sc.nextInt();
 
-        System.out.println("4. Input the queries elements: ");
+        // System.out.println("4. Input the queries elements: ");
         for (z = 0; z <= m; z++) {
             // Take input: elements of queries
             queries.add(sc.nextLine());
         }
 
         // to Test the input
-        System.out.print("5. Size of the strings are: ");
-        System.out.println(n);
-        System.out.print("6. Elements of the strings are: ");
-        System.out.println(strings);
-        System.out.print("7. Size of the queries are: ");
-        System.out.println(m);
-        System.out.print("8. Elements of the queries are: ");
-        System.out.println(queries);
-
-        // Write your code here
-
         /*
-         * Map<String, Long> collect = strings.stream().collect( groupingBy(
-         * Function.identity(), counting() ) );
-         * 
-         * }
+         * System.out.print("5. Size of the strings are: "); System.out.println(n);
+         * System.out.print("6. Elements of the strings are: ");
+         * System.out.println(strings);
+         * System.out.print("7. Size of the queries are: "); System.out.println(m);
+         * System.out.print("8. Elements of the queries are: ");
+         * System.out.println(queries);
          */
 
         // Count queries elements
-        Map<String, Integer> counts = queries.parallelStream()
-                .collect(Collectors.toConcurrentMap(w -> w, w -> 1, Integer::sum));
-        System.out.print("9. Count the number of queries elements: ");
-        System.out.println(counts);
-
         /*
-         * Set<String> set = new LinkedHashSet<>(); set.addAll(list);
-         * 
-         * for (String s : set) {
-         * 
-         * System.out.println(s + " : " + Collections.frequency(list, s)); }
-         */
-
-        /*
-         * System.out.println("Freq of queries in strings: " +
-         * Collections.frequency(strings, "abc"));
+         * Map<String, Integer> counts = queries.parallelStream()
+         * .collect(Collectors.toConcurrentMap(w -> w, w -> 1, Integer::sum));
+         * System.out.print("9. Count the number of queries elements: ");
+         * System.out.println(counts);
          */
 
         // Create a new list that contain the same elements from both list
         List<String> sameElements = new ArrayList<>(strings);
         sameElements.retainAll(queries);
-        System.out.print("10. Queries elements in strings: ");
-        System.out.println(sameElements);
+        // System.out.print("10. Queries elements in strings: ");
+        // System.out.println(sameElements);
+
+        /*
+         * System.out.print("11. Count the number of sameELements elements: ");
+         * System.out.println(countsElements);
+         * System.out.println("12. The answers is: "); countsElements.forEach((key,
+         * value) -> System.out.println(countsElements.get(key)));
+         */
 
         // Count the number of sameElements elements
         Map<String, Integer> countsElements = sameElements.parallelStream()
                 .collect(Collectors.toConcurrentMap(w -> w, w -> 1, Integer::sum));
-        System.out.print("11. Count the number of sameELements elements: ");
-        System.out.println(countsElements);
+
+        // Take only second to last index
+        ArrayList<Integer> valueList = new ArrayList<Integer>(countsElements.values());
+        int arrayLength = countsElements.size();
+        ArrayList<Integer> sublist = new ArrayList<Integer>(valueList.subList(1, arrayLength));
+
+        // Print the result
+        // System.out.println("14. The final result is: ");
+        sublist.forEach((c) -> System.out.println(c));
+
     }
 
     public static void main(String[] args) throws Exception {
